a70b69c36bf467f77474275e1153956e53460ae8
KaiLindholm <kailindholm20022@gmail.com>
2023-04-11 12:18:02 -0500

    Initial Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * EmbeddedLab5.c
+ *
+ * Created: 4/11/2023 12:12:04 PM
+ * Author : Kai Lindholm & James Ostrowski
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

79596465952f08aad3bb3c82c24e2fd170fd584b
KaiLindholm <kailindholm20022@gmail.com>
2023-04-17 00:38:12 -0500

    Parsed in sin csv file

--- Test.c
+++ Test.c
@@ -2,22 +2,54 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define SIZE 1024
 
 void read_command(char * command);
 void Gcommand();
 void Wcommand(uint8_t dac, uint8_t freq, uint8_t cycles);
 void Scommand(uint8_t dac, float voltage);
+uint8_t read_wave_form_file(char * file_path);
+uint8_t read_wave_form_file(char * file_path){
+	FILE* fp; 
+	fp = fopen(file_path, "r"); // read from file_path
+
+	if( fp == NULL ) { 		// file path does not exist
+		return 1; 
+	}
+
+	fseek(fp, 0, SEEK_END);		// go to the end of the file
+	uint8_t size = ftell(fp);	// find out the size of the buffer needed. 
+	char * buffer = (char * ) malloc((size + 1) * sizeof(char));
+	printf("%d\n", size);
+	//uint8_t * wave_form = (uint8_t * ) malloc((size + 1) * sizeof(uint8_t));
+	uint8_t wave_form[64] = {0};
+	fseek(fp, 0, SEEK_SET);	// go back to start of file. 
+	uint8_t i = 0; 
+	while(!feof(fp) ){
+		fgets(buffer, SIZE, fp); 
+		char * tokens = strtok(buffer, ",");
+		while(tokens != NULL) {
+			wave_form[i] = atoi(tokens); 
+			printf("Voltage (%d): %d V\n", (i+1), wave_form[i]);
+			tokens = strtok(NULL, ",");
+
+			++i; 
+		}
+	}
+	
+
+	//fclose(fp);
+	return 0; 
+}
+
 
 int main(void)
 {
-    char c[2] = "G\0";
-	char w[8] = "W,1,2,3\0";
-	char s[6] = "S,1,2\0";
-    read_command(c);
-	printf("\n");
-	read_command(w);
-	printf("\n");
-	read_command(s);
+    if(read_wave_form_file("sin_table.csv")){
+		printf("ERR\n");
+		return 1; 
+	}
+
     return 0;
 }
 
